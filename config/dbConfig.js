d3e7acf7293dae6cc9dcb89517ea1630a5d54241
YousefDahish <yousefdahish32@gmail.com>
2023-12-01 20:39:16 +0200

    DB URL

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,4 +1,5 @@
 module.exports = {
     RENDER_URL:
-        "postgres://pet_db_s0l4_user:Zgp2UIvHfMjyTEEZ4iYvz24dWFSB5BDy@dpg-chb3rubhp8u01673dho0-a.oregon-postgres.render.com/pet_db_s0l4?ssl=true",
+        "postgres://graduation_db_5z23_user:qJoUC1hxTuHAZTDqUf1WimbHXb5rDRrF@dpg-cll22tsjtl8s73f5t840-a/graduation_db_5z23",
 }
+//postgres://pet_db_s0l4_user:Zgp2UIvHfMjyTEEZ4iYvz24dWFSB5BDy@dpg-chb3rubhp8u01673dho0-a.oregon-postgres.render.com/pet_db_s0l4?ssl=true
